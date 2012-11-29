5286aebbfb33b9e9fdbb06917afda6c1f87fd8ac
Steve Vinoski <vinoski@ieee.org>
2012-11-29 11:15:54 -0500

    update to use riak_core 1.2.1

--- apps/riak_id/src/riak_id_app.erl
+++ apps/riak_id/src/riak_id_app.erl
@@ -10,7 +10,7 @@
 start(_StartType, _StartArgs) ->
     case riak_id_sup:start_link() of
         {ok, Pid} ->
-            ok = riak_core:register_vnode_module(riak_id_vnode),
+            ok = riak_core:register(riak_id, [{vnode_module, riak_id_vnode}]),
             ok = riak_core_ring_events:add_guarded_handler(riak_id_ring_event_handler, []),
             ok = riak_core_node_watcher_events:add_guarded_handler(riak_id_node_event_handler, []),
             ok = riak_core_node_watcher:service_up(riak_id, self()),
